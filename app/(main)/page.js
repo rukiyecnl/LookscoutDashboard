154df5ba6b52b3f49f4e9a6c0300d1ed109b113b
Rukiye Canlı <canli.rukiye@std.izu.edu.tr>
2024-08-26 00:12:25 +0300

    changes2

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -6,10 +6,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Content from "../components/Content";
 import SideBarContent from "../components/SideBarContent";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [displayStatus, setDisplayStatus] = useState("d-none");
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 990) {
+        setDisplayStatus("d-none");
+      } else {
+        setDisplayStatus(""); // Eğer d-none yerine başka bir değer kullanmak istiyorsanız burayı ayarlayın
+      }
+    };
+
+    // Component ilk yüklendiğinde boyutu kontrol et
+    handleResize();
+
+    // Tarayıcı boyutu değiştikçe kontrol etmeye devam et
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener'ı kaldırır
+    return () => window.removeEventListener("resize", handleResize);
+
+  }, []);
 
 
   return (
@@ -18,7 +38,7 @@ export default function Home() {
         <div className="d-none d-lg-flex "  >
           <SideBarIcons displayStatus={displayStatus} setDisplayStatus={setDisplayStatus}/>
         </div>
-        <div className="" style={{width: `${displayStatus=="d-none" ? "100%" : "93%"}`}} >
+        <div className="" style={{width: `${displayStatus=="d-none" ? "100%" : "72%"}`}} >
           <Content />
         </div>
       </div>
